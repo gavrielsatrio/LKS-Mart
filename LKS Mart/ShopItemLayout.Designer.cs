e74de2f2e8b1242a17cbc3e16cacbf5dad26d434
Gavriel Satrio Widjaya <gavriel228@gmail.com>
2022-02-25 18:00:56 +0700

    Add : Shop item layout
    With default design (color, layout, background)

---
+++ LKS Mart/ShopItemLayout.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace LKS_Mart
+{
+    partial class ShopItemLayout
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutShopItem = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.lblStock = new System.Windows.Forms.Label();
+            this.btnAddToCart = new System.Windows.Forms.Button();
+            this.tableLayoutShopItem.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutShopItem
+            // 
+            this.tableLayoutShopItem.ColumnCount = 5;
+            this.tableLayoutShopItem.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutShopItem.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 35F));
+            this.tableLayoutShopItem.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutShopItem.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutShopItem.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutShopItem.Controls.Add(this.btnAddToCart, 4, 0);
+            this.tableLayoutShopItem.Controls.Add(this.lblStock, 3, 0);
+            this.tableLayoutShopItem.Controls.Add(this.lblPrice, 2, 0);
+            this.tableLayoutShopItem.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutShopItem.Controls.Add(this.lblName, 1, 0);
+            this.tableLayoutShopItem.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutShopItem.Location = new System.Drawing.Point(16, 16);
+            this.tableLayoutShopItem.Name = "tableLayoutShopItem";
+            this.tableLayoutShopItem.RowCount = 1;
+            this.tableLayoutShopItem.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutShopItem.Size = new System.Drawing.Size(468, 73);
+            this.tableLayoutShopItem.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(67, 67);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblName
+            // 
+            this.lblName.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblName.Location = new System.Drawing.Point(76, 0);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(122, 73);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "<< Product Name >>";
+            this.lblName.UseCompatibleTextRendering = true;
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblPrice.Location = new System.Drawing.Point(204, 0);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(86, 73);
+            this.lblPrice.TabIndex = 2;
+            this.lblPrice.Text = "<< Product Price >>";
+            this.lblPrice.UseCompatibleTextRendering = true;
+            // 
+            // lblStock
+            // 
+            this.lblStock.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblStock.Location = new System.Drawing.Point(296, 0);
+            this.lblStock.Name = "lblStock";
+            this.lblStock.Size = new System.Drawing.Size(67, 73);
+            this.lblStock.TabIndex = 3;
+            this.lblStock.Text = "<< Product Stock >>";
+            this.lblStock.UseCompatibleTextRendering = true;
+            // 
+            // btnAddToCart
+            // 
+            this.btnAddToCart.BackColor = System.Drawing.Color.Black;
+            this.btnAddToCart.FlatAppearance.BorderSize = 0;
+            this.btnAddToCart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAddToCart.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddToCart.ForeColor = System.Drawing.Color.White;
+            this.btnAddToCart.Location = new System.Drawing.Point(369, 3);
+            this.btnAddToCart.Name = "btnAddToCart";
+            this.btnAddToCart.Size = new System.Drawing.Size(96, 35);
+            this.btnAddToCart.TabIndex = 9;
+            this.btnAddToCart.Text = "Add to Cart";
+            this.btnAddToCart.UseVisualStyleBackColor = false;
+            // 
+            // ShopItemLayout
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.tableLayoutShopItem);
+            this.Name = "ShopItemLayout";
+            this.Padding = new System.Windows.Forms.Padding(16);
+            this.Size = new System.Drawing.Size(500, 105);
+            this.tableLayoutShopItem.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutShopItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Label lblStock;
+        private System.Windows.Forms.Button btnAddToCart;
+    }
+}
